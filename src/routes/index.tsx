cbe8ed8cf9acb71906c67ebba8da3335cd05b2e3
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-05-28 19:30:55 -0300

    chore: add public and private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { CgSpinnerTwo } from "react-icons/cg";
 
 import { Dashboard } from "@/layout";
+import { Private } from "./private";
+import { Public } from "./public";
+import SignIn from "@/pages/auth/sign-in";
 
 const HomeRouter = lazy(() =>
   import("@/pages/dashboard/home/router").then((module) => ({
@@ -27,10 +30,16 @@ export function Router(): ReactElement {
       }
     >
       <Routes>
-        <Route element={<Dashboard />}>
-          <Route index element={<Navigate to="/home" />} />
-          <Route path="home/*" element={<HomeRouter />} />
-          <Route path="invoices/*" element={<InvoicesRouter />} />
+        <Route element={<Public />}>
+          <Route index element={<Navigate to="/sign-in" />} />
+          <Route path="sign-in" element={<SignIn />} />
+        </Route>
+
+        <Route element={<Private />}>
+          <Route element={<Dashboard />}>
+            <Route path="home/*" element={<HomeRouter />} />
+            <Route path="invoices/*" element={<InvoicesRouter />} />
+          </Route>
         </Route>
       </Routes>
     </Suspense>
